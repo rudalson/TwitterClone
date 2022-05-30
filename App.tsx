fc1b09018c427dfbaa78274dc4299c257888436f
rudalson <rudalson@gmail.com>
2022-05-30 19:56:35 +0900

    added : auth, updateuser

--- App.tsx
+++ App.tsx
@@ -1,18 +1,55 @@
+import React, { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { Amplify } from 'aws-amplify';
+import { Amplify, Auth, API, graphqlOperation } from 'aws-amplify';
 import { withAuthenticator } from 'aws-amplify-react-native';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 import awsconfig from './aws-exports';
+import { getUser } from './graphql/queries';
+import { createUser } from './graphql/mutations';
 
 Amplify.configure(awsconfig);
 
 function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+
+  const saveUserToDB = async (user) => {
+    await API.graphql(graphqlOperation(createUser, { input: user }));
+  };
+
+  useEffect(() => {
+    const updateUser = async () => {
+      const userInfo = await Auth.currentAuthenticatedUser({
+        bypassCache: true,
+      });
+
+      if (userInfo) {
+        const userData = await API.graphql(
+          graphqlOperation(getUser, { id: userInfo.attributes.sub })
+        );
+        console.log(userData);
+        if (userData?.data?.getUser) {
+          console.log('user alerady exists');
+        } else {
+          const user = {
+            id: userInfo.attributes.sub,
+            username: userInfo.username,
+            name: userInfo.username,
+            email: userInfo.attributes.email,
+            image:
+              'https://cdn.eyesmag.com/content/uploads/posts/2020/12/09/tesla-elon-musk-moves-to-texas-1-5b119051-2c23-449b-b554-7445558ebf62.jpg',
+          };
+          await saveUserToDB(user);
+        }
+      }
+    };
+
+    updateUser();
+  }, []);
 
   if (!isLoadingComplete) {
     return null;
