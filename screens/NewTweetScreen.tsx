1fa6b16cec0738e786067f4d6940d75fa900f260
rudalson <rudalson@gmail.com>
2022-06-03 19:29:09 +0900

    added : post a tweet 완료. backend 쪽으로 트윗 저장까지

--- screens/NewTweetScreen.tsx
+++ screens/NewTweetScreen.tsx
@@ -5,35 +5,54 @@ import {
   SafeAreaView,
   TextInput,
 } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { API, graphqlOperation, Auth } from 'aws-amplify';
 import { AntDesign } from '@expo/vector-icons';
 
+import { createTweet } from '../graphql/mutations';
 import { Text, View } from '../components/Themed';
 import Colors from '../constants/Colors';
-import Feed from '../components/Feed';
-
-import tweets from '../data/tweets';
 import ProfilePicture from '../components/ProfilePicture';
+import Feed from '../components/Feed';
+import tweets from '../data/tweets';
 
 export default function NewTweetScreen() {
   const [tweet, setTweet] = useState('');
   const [imageUrl, setImageUrl] = useState('');
 
-  const onPostTweet = () => {
-    console.log('Posting the tweet: ', tweet);
-    console.log('Posting the imageurl: ', imageUrl);
+  const navigation = useNavigation();
+
+  const onPostTweet = async () => {
+    try {
+      const currentUser = await Auth.currentAuthenticatedUser({
+        bypassCache: true,
+      });
+      const newTweet = {
+        content: tweet,
+        image: imageUrl,
+        userTweetsId: currentUser.attributes.sub,
+      };
+      await API.graphql(graphqlOperation(createTweet, { input: newTweet }));
+
+      navigation.goBack();
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.headerContainer}>
-        <AntDesign name="close" size={30} color={Colors.light.tint} />
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <AntDesign name='close' size={30} color={Colors.light.tint} />
+        </TouchableOpacity>
         <TouchableOpacity style={styles.button} onPress={onPostTweet}>
           <Text style={styles.buttonText}>Tweet</Text>
         </TouchableOpacity>
       </View>
 
       <View style={styles.newTweetContainer}>
-        <ProfilePicture image="https://cdn.eyesmag.com/content/uploads/posts/2020/12/09/tesla-elon-musk-moves-to-texas-1-5b119051-2c23-449b-b554-7445558ebf62.jpg" />
+        <ProfilePicture image='https://cdn.eyesmag.com/content/uploads/posts/2020/12/09/tesla-elon-musk-moves-to-texas-1-5b119051-2c23-449b-b554-7445558ebf62.jpg' />
         <View style={styles.inputsContainer}>
           <TextInput
             value={tweet}
