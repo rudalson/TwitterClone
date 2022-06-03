02eb9785df1318b48704b975d4fab6f4409cfa6b
rudalson <rudalson@gmail.com>
2022-06-03 20:12:26 +0900

    added : list the db tweets not dummy

--- components/Feed/index.tsx
+++ components/Feed/index.tsx
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, FlatList } from 'react-native';
+import { API, graphqlOperation } from 'aws-amplify';
 
+import { listTweets } from '../../graphql/queries';
 import Tweet from '../Tweet';
-import tweets from '../../data/tweets';
 
-const Feed = () => (
-  <View style={{ width: '100%' }}>
-    <FlatList
-      data={tweets}
-      renderItem={({ item }) => <Tweet tweet={item} />}
-      keyExtractor={(item) => item.id}
-    />
-  </View>
-);
+const Feed = () => {
+  const [tweets, setTweets] = useState([]);
+  const [loading, setLoading] = useState(false);
 
+  const fetchTweets = async () => {
+    setLoading(true);
+    try {
+      const tweetsData = await API.graphql(graphqlOperation(listTweets));
+      setTweets(tweetsData?.data?.listTweets.items);
+    } catch (e) {
+      console.warn(e);
+    } finally {
+      setLoading(false);
+    }
+  };
 
+  useEffect(() => {
+    fetchTweets();
+  }, []);
+
+  return (
+    <View style={{ width: '100%' }}>
+      <FlatList
+        data={tweets}
+        renderItem={({ item }) => <Tweet tweet={item} />}
+        keyExtractor={(item) => item.id}
+        refreshing={loading}
+        onRefresh={fetchTweets}
+      />
+    </View>
+  );
+};
 
 export default Feed;
