71aa094a7594b3eec47f48b3326b786615958d77
rudalson <rudalson@gmail.com>
2022-06-14 18:28:43 +0900

    added : amplify storage & imagepicker

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
+    },
+    "storage": {
+        "TwitterClone": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
